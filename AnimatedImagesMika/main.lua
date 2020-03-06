a0a2db55a62036984d68e3183d014ec1c7c27fd3
Mika Roy <mika.roy-rafuse@ocsbstudent.ca>
2020-03-06 13:44:41 -0500

    Added Touch&React

--- AnimatedImagesMika/main.lua
+++ AnimatedImagesMika/main.lua
@@ -1,7 +1,18 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: Animated Images
+-- Name:mika roy-rafuse
+--course ICS20/3c
+--this program will display an animated image that move imagecross the screen
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--hide the status bar
+display.setStatusBar(display.hiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Image/ocean.jpg". 2048, 1536)
+
+--character image with width height
+local waluigi
